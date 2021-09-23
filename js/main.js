5ae90f12ed15e23a5ca037e45ae7d2ad154f7278
Karine Gasesyan <karinegasesyan@gmail.com>
2021-09-23 16:00:53 +0400

    upds 09/23/2021

--- js/main.js
+++ js/main.js
@@ -2,18 +2,28 @@ window.onload = function() {
 
     // toggle header
     const btnHumbergur = document.querySelector('#btnHamberger');
+    const body = document.querySelector('body');
     const header = document.querySelector('.header');
-    const overlay = document.querySelector('.overlay');
+   // const overlay = document.querySelector('.overlay');
+    const hasFade = document.querySelectorAll('.has-fade');
 
     btnHumbergur.addEventListener("click", function(e){ 
         if(header.classList.contains('open')){ // close menu
+            body.classList.remove('noscroll');
             header.classList.remove('open');
-            overlay.classList.remove('fade-in');
-            overlay.classList.add('fade-out');
+            hasFade.forEach(function(el){
+                el.classList.remove('fade-in');
+                el.classList.add('fade-out');
+            });
+            
         }else{
+            body.classList.add('noscroll');
             header.classList.add('open');
-            overlay.classList.remove('fade-out');
-            overlay.classList.add('fade-in');
+            hasFade.forEach(function(el){
+                el.classList.remove('fade-out');
+                el.classList.add('fade-in');
+            });
+
         }   
      });
 
